4aef584940c0b17985967a60dd1aff4386e35ec2
edward <edwardlfish@gmail.com>
2024-08-20 21:00:05 +0300

    accounting was started

---
+++ src/main/java/telran/java53/accounting/dto/UserEditDto.java
@@ -0,0 +1,9 @@
+package telran.java53.accounting.dto;
+
+import lombok.Getter;
+
+@Getter
+public class UserEditDto {
+	String firstName;
+	String lastName;
+}
---
+++ src/main/java/telran/java53/accounting/service/UserAccountService.java
@@ -0,0 +1,20 @@
+package telran.java53.accounting.service;
+
+import telran.java53.accounting.dto.RolesDto;
+import telran.java53.accounting.dto.UserDto;
+import telran.java53.accounting.dto.UserEditDto;
+import telran.java53.accounting.dto.UserRegisterDto;
+
+public interface UserAccountService {
+	UserDto register(UserRegisterDto userRegisterDto);
+
+	UserDto getUser(String login);
+
+	UserDto removeUser(String login);
+
+	UserDto updateUser(String login, UserEditDto userEditDto);
+
+	RolesDto changeRolesList(String login, String role, boolean isAddRoles);
+
+	void changePassword(String login, String newPassword);
+}
