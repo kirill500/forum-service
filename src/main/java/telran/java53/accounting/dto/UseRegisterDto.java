20990c26fdf42450056dcfc3eb1523e7501158ac
Kirillgutsalo <kirya.sever11@gmail.com>
2024-08-27 00:22:19 +0300

    Accounting was started

---
+++ src/main/java/telran/java53/accounting/dto/UseRegisterDto.java
@@ -0,0 +1,11 @@
+package telran.java53.accounting.dto;
+
+import lombok.Getter;
+
+@Getter
+public class UseRegisterDto {
+	String login;
+    String password;
+    String firstName;
+    String lastName;
+}
---
+++ src/main/java/telran/java53/accounting/service/UserAccountService.java
@@ -0,0 +1,5 @@
+package telran.java53.accounting.service;
+
+public interface UserAccountService {
+
+}
