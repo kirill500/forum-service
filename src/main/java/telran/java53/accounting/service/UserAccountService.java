661a539d7fa1633e9ff1744292b87d3cae3a2e9a
Kirillgutsalo <kirya.sever11@gmail.com>
2024-08-27 00:24:41 +0300

    added accounting interface

--- src/main/java/telran/java53/accounting/service/UserAccountService.java
+++ src/main/java/telran/java53/accounting/service/UserAccountService.java
@@ -1,5 +1,20 @@
 package telran.java53.accounting.service;
 
-public interface UserAccountService {
+import telran.java53.accounting.dto.RolesDto;
+import telran.java53.accounting.dto.UseRegisterDto;
+import telran.java53.accounting.dto.UserDto;
+import telran.java53.accounting.dto.UserEditDto;
 
+public interface UserAccountService {
+	UserDto register(UseRegisterDto useRegisterDto);
+	
+	UserDto getUser(String login);
+	
+	UserDto removeUser(String login);
+	
+	UserDto updateUser(String login, UserEditDto userEditDto);
+	
+	RolesDto changeRolesList(String login, String role, boolean isAddRoles);
+	
+	void changePassword(String login, String newPassword);
 }
